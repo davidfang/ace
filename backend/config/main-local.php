fe13005ccc4f07419b1dba83533763acc504eb9f
davidfang <mw_fang@163.com>
2016-01-18 17:19:42 +0800

    添加gi自定义gii模板操作配置

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -15,7 +15,51 @@ if (!YII_ENV_TEST) {
     $config['modules']['debug'] = 'yii\debug\Module';
 
     $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
+    //$config['modules']['gii'] = 'yii\gii\Module';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'generators'=>[
+            'controller' => [
+                'class' => 'zc\gii\controller\Generator',
+                'templates' => [
+                    'zc-gii' => '@vendor/zc/gii/controller/default',
+                ]
+            ],
+            'crud' => [
+                //'class' => 'yii\gii\generators\crud\Generator',
+                'class' => 'zc\gii\crud\Generator',
+                'templates' => [
+                    'zc-gii' => '@vendor/zc/gii/crud/default',
+                ]
+            ],
+            'module' => [
+                'class' => 'zc\gii\module\Generator',
+                'templates' => [
+                    'zc-gii' => '@vendor/zc/gii/module/default',
+                ]
+            ],
+            'form' => [
+                'class' => 'zc\gii\form\Generator',
+                'templates' => [
+                    'zc-gii' => '@vendor/zc/gii/form/default',
+                ]
+            ],
+            'model' => [
+                'class' => 'zc\gii\model\Generator',
+                'templates' => [
+                    'zc-gii' => '@vendor/zc/gii/model/default',
+                ]
+            ],
+            'extension' => [
+                'class' => 'zc\gii\extension\Generator',
+                'templates' => [
+                    'zc-gii' => '@vendor/zc/gii/extension/default',
+                ]
+            ],
+
+        ]
+
+    ];
 }
 
 return $config;
